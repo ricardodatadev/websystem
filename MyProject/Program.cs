1ada64411d5a003f31730b2d4e3d3451a053eb18
Ricardo Pereira <ricardodatadev@gmail.com>
2026-01-15 01:12:19 -0300

    feat(DepartmentsController): CRUD Scaffolding

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyProject.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MyProjectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyProjectContext") ?? throw new InvalidOperationException("Connection string 'MyProjectContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
