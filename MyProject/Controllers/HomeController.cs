29bfa994e89ec5e54cb9835f06d65bcd6710f0d9
Ricardo Pereira <ricardodatadev@gmail.com>
2026-01-15 00:47:05 -0300

    chore: move .gitignore to repository root

--- MyProject/Controllers/HomeController.cs
+++ MyProject/Controllers/HomeController.cs
@@ -11,15 +11,25 @@ namespace MyProject.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {    ViewData["Message"] = "Salles Web MVC App";
+             ViewData["Developer"] = "Ricardo Pereira";
+
+            return View();
+        }
+
         public IActionResult Privacy()
         {
+            ViewData["Message"] = "Metallica.";
+            ViewData["email"] = "ricardodatadev@gmail.com";
+
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new MyProject.Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
